b2f867bffd6f7c0596f3976f2cb7f0c8d2ff8812
SaraDonzellini <saradonzie@gmail.com>
2023-02-03 15:28:58 +0100

    added object 'Movies'

--- models/Movies.php
+++ models/Movies.php
@@ -3,11 +3,20 @@
 class Movies {
   public $title;
   public $year;
+  public $genre;
+  public $director;
 
 
-
-  function __construct($_)
+  function __construct($_title, $_year, $_genre, $_director)
   {
-    
+    $this->title = $_title;
+    $this->year = $_year;
+    $this->genre = $_genre;
+    $this->director = $_director;
+  }
+
+
+  public function GetTitle(){
+    return $this->title;
   }
 }
